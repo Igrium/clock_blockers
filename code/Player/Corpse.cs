aa545dc22482d7d348264cc36f813c1d60e3103f
Sam54123 <thesam54123@gmail.com>
2023-07-28 00:00:45 -0700

    players no longer collide with ragdolls

--- code/Player/Corpse.cs
+++ code/Player/Corpse.cs
@@ -4,8 +4,14 @@ namespace ClockBlockers;
 
 public partial class Corpse : ModelEntity
 {
+	public override void Spawn()
+	{
+		base.Spawn();
+		Tags.Add( "ragdoll" );
+	}
+
 	public DamageInfo KillDamage { get; set; }
-	[Net] public Entity Attacker { get; set; }
-	[Net] public Entity Weapon { get; set; }
-	[Net] public IClient OwnerClient { get; set; }
+	[Net] public Entity? Attacker { get; set; }
+	[Net] public Entity? Weapon { get; set; }
+	[Net] public IClient? OwnerClient { get; set; }
 }
